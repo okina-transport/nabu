546cbbaee0088c9058553b2bf606a53bc6cc004a
fmorisse <fmorisse@okina.fr>
2021-11-17 13:50:33 +0100

    Ajout rôle API + création version de test + Add read role to get method + Remise POM correct

---
+++ src/main/java/no/rutebanken/nabu/helper/organisation/APIAuthorizationConstants.java
@@ -0,0 +1,8 @@
+package no.rutebanken.nabu.helper.organisation;
+
+public class APIAuthorizationConstants {
+    public static final java.lang.String READ_ROUTE_DATA = "readRouteData";
+
+    public APIAuthorizationConstants() {
+    }
+}
--- src/main/java/no/rutebanken/nabu/rest/TimeTableJobEventResource.java
+++ src/main/java/no/rutebanken/nabu/rest/TimeTableJobEventResource.java
@@ -36,10 +36,10 @@ import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static no.rutebanken.nabu.helper.organisation.APIAuthorizationConstants.READ_ROUTE_DATA;
 import static no.rutebanken.nabu.rest.mapper.EnumMapper.convertEnums;
 import static org.rutebanken.helper.organisation.AuthorizationConstants.ROLE_ROUTE_DATA_ADMIN;
 import static org.rutebanken.helper.organisation.AuthorizationConstants.ROLE_ROUTE_DATA_EDIT;
-
 
 @Component
 @Produces("application/json")
@@ -59,7 +59,7 @@ public class TimeTableJobEventResource {
 
     @GET
     @Path("/{providerId}")
-    @PreAuthorize("hasRole('" + ROLE_ROUTE_DATA_ADMIN + "') or @providerAuthenticationService.hasRoleForProvider(authentication,'" + ROLE_ROUTE_DATA_EDIT + "',#providerId)")
+    @PreAuthorize("hasRole('" + ROLE_ROUTE_DATA_ADMIN + "') or hasRole('" + READ_ROUTE_DATA + "') or @providerAuthenticationService.hasRoleForProvider(authentication,'" + ROLE_ROUTE_DATA_EDIT + "',#providerId)")
     public List<JobStatus> listStatus(@PathParam("providerId") Long providerId, @QueryParam("from") String from,
                                       @QueryParam("to") String to, @QueryParam("action") List<String> actions,
                                       @QueryParam("state") List<JobStatus.State> states, @QueryParam("chouetteJobId") List<Long> jobIds,
@@ -107,7 +107,7 @@ public class TimeTableJobEventResource {
     }
 
     @GET
-    @PreAuthorize("hasRole('" + ROLE_ROUTE_DATA_ADMIN + "')")
+    @PreAuthorize("hasRole('" + ROLE_ROUTE_DATA_ADMIN + "') or hasRole('" + READ_ROUTE_DATA + "')")
     public List<JobStatus> listStatus(@QueryParam("from") String from,
                                       @QueryParam("to") String to, @QueryParam("action") List<String> actions,
                                       @QueryParam("state") List<JobStatus.State> states, @QueryParam("chouetteJobId") List<Long> jobIds,
